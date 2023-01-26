1b322dfd1d8c446992cfd1db8d9d6943bfd68813
Andrew Bateman <gomezbateman@yahoo.com>
2023-01-26 21:55:43 +0100

    refactor: remove unrequired files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  template: `<ion-app>
+  <ion-router-outlet></ion-router-outlet>
+</ion-app>`
 })
 export class AppComponent {
   constructor(
